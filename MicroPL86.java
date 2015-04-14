a0b9fa441f33ad7f1496939d98a3bb86e62686c5
vragosta <allchenzo@gmail.com>
2015-04-14 17:08:39 -0400

    MicroPL86 Lexical Analyzer

---
+++ MicroPL86.java
@@ -0,0 +1,28 @@
+import java.io.*;
+
+public class MicroPL86 {
+    public static void main(String [] args) throws Exception{
+        processCommandLine(args);
+        
+        if(filename == null){
+            System.out.println("Usage: MicroPL86 <filename>");
+            System.exit(2);
+        }
+        
+        Lexer lexer = new Lexer(new File(filename));
+        while(lexer.token().type != Token.EOI){
+            System.out.println(lexer.token());
+            lexer.next();
+        }
+    }
+    
+    static void processCommandLine(String [] args){
+        for(int i=0; i < args.length; i++)
+            if(args[i].startsWith("-")){
+            }
+            else 
+                filename = args[i];
+    }
+            static String filename = null;
+    }
+    
